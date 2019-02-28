8cdfd9a825a6a929bbd8d9a555660bd4015defc0
yoshiyaohno <yoshiyaohno@gmail.com>
2019-02-28 15:26:59 -0500

    MATRIX MULTIPLICATION WORKS

--- line.hs
+++ line.hs
@@ -1,16 +1,21 @@
 module Line where
 
+import           Control.Applicative
 import qualified Data.Set        as S
 import qualified Data.List       as L
 import qualified Data.Map.Strict as M
 
-data Line   = Line Point Point deriving (Show)
-data Point  = Point {getX::Int, getY::Int, getZ::Int, getQ::Int}
-                deriving (Eq, Ord)
+data Line = Line (Vect Int) (Vect Int) deriving (Show)
+data Vect a = Vect { getX::a
+                   , getY::a
+                   , getZ::a
+                   , getQ::a
+                   } deriving (Eq, Ord)
 data Color  = Color {r::Int, g::Int, b::Int}
 
-type Screen = M.Map Point Color
+type Screen = M.Map (Vect Int) Color
 type DrawAction = Screen -> Screen
+type Transform a = Vect (Vect a)
 
 blk = Color 0 0 0
 red = Color 255 0 0
@@ -20,11 +25,38 @@ grn = Color 0 255 0
 -- Prelude.pi is a Floating and not a Fractional and that's stupid
 _pi = 3.14159
 
-instance Show Point where
-    show (Point x y z q) = "(" ++ show x ++ ", " ++ show y ++ ")"
+instance (Show t) => Show (Vect t) where
+    show (Vect x y z q) = "("
+                            ++ show x ++ ", "
+                            ++ show y ++ ", "
+                            ++ show z ++ ", "
+                            ++ show q
+                            ++ ")"
+
 -- the duality of man
 instance Show Color where
     show = unwords . map show . ([r, g, b] <*>) . pure
+
+instance Functor Vect where
+    fmap f (Vect x y z q) = (Vect (f x) (f y) (f z) (f q))
+    
+instance Applicative Vect where
+    pure x = (Vect x x x x)
+    (Vect f0 f1 f2 f3) <*> (Vect x y z q) =
+        (Vect (f0 x) (f1 y) (f2 z) (f3 q))
+
+instance Foldable Vect where
+    foldr f acc (Vect x0 x1 x2 x3) =
+        foldr f acc [x0, x1, x2, x3]
+
+mmult :: (Num a, Functor f) => Transform a -> f (Vect a) -> f (Vect a)
+mmult t = fmap (pmult t)
+
+pmult :: (Num a) => Transform a -> Vect a -> Vect a
+pmult t = liftA2 (dot) t . pure
+
+dot :: (Num a) => Vect a -> Vect a -> a
+dot p = sum . liftA2 (*) p
 
 ppmHeader :: (Int, Int) -> String
 ppmHeader (w, h) = "P3 " ++ show w ++ " " ++ show h ++ " 255\n"
@@ -37,7 +69,7 @@ allPairs (x:xs) = map ((,) x) xs ++ allPairs xs
 -- look at me actually writing comments
 
 --main = do
---    let points  = polygon 60 255 (Point 470 470)
+--    let points  = polygon 60 255 (Vect 470 470)
 --        lines   = [drawLine (Line p0 p1)
 --                            (Color  ((getX p0 + getX p1) `div` 4)
 --                                    ((getY p0 + getY p1) `div` 4)
@@ -47,9 +79,9 @@ allPairs (x:xs) = map ((,) x) xs ++ allPairs xs
 --    writeFile "out.ppm" (printPixels (900, 900) drawing)
 
 -- floating point math :'(
-polygon :: (Integral a) => a -> a -> Point -> [Point]
-polygon s r (Point x y z q) =
-    [Point  (x + round (fromIntegral r * sin ((fromIntegral i) * th)))
+polygon :: (Integral a) => a -> a -> Vect a -> [Vect a]
+polygon s r (Vect x y z q) =
+    [Vect  (x + round (fromIntegral r * sin ((fromIntegral i) * th)))
             (y + round (fromIntegral r * cos ((fromIntegral i) * th)))
             0 1
         | i <- [0..s-1]]
@@ -61,7 +93,7 @@ polygon s r (Point x y z q) =
 printPixels :: (Int, Int) -> Screen -> String
 printPixels (w, h) pxs =
     ppmHeader (w, h)
-    ++ (unlines . map unwords $ [[show . f $ M.lookup (Point x y 0 1) pxs
+    ++ (unlines . map unwords $ [[show . f $ M.lookup (Vect x y 0 1) pxs
                 | x <- [0..w-1]] | y <- [0..h-1]])
     where   f Nothing  = Color 0 0 0
             f (Just c) = c 
@@ -73,7 +105,7 @@ drawLine l c scrn = foldr insC scrn (rasterLine l)
     where insC = flip M.insert c
 
 -- just gives you the points a line covers, no color
-rasterLine :: Line -> [Point]
+rasterLine :: Line -> [Vect Int]
 rasterLine (Line p0 p1)
     | dy == 0 && dx == 0        = []
     | abs dx > abs dy && dx > 0 = _rLx (Line p0 p1)
@@ -85,16 +117,16 @@ rasterLine (Line p0 p1)
 
 -- hell yeah ugly helper functions
 --  (I could use only one by flipping the tuples somehow but ergh)
-_rLx :: Line -> [Point]
-_rLx (Line (Point x0 y0 _ _) (Point x1 y1 _ _)) =
-    L.zipWith4 (Point) [x0..x1] ys (repeat 0) (repeat 1)
+_rLx :: Line -> [Vect Int]
+_rLx (Line (Vect x0 y0 _ _) (Vect x1 y1 _ _)) =
+    L.zipWith4 (Vect) [x0..x1] ys (repeat 0) (repeat 1)
     where   ys = map ((+y0) . (`quot` (2* abs dx))) . tail $ [negate dy, dy..]
             dy = y1 - y0
             dx = x1 - x0
 
-_rLy :: Line -> [Point]
-_rLy (Line (Point x0 y0 _ _) (Point x1 y1 _ _)) =
-    L.zipWith4 (Point) xs [y0..y1] (repeat 0) (repeat 1)
+_rLy :: Line -> [Vect Int]
+_rLy (Line (Vect x0 y0 _ _) (Vect x1 y1 _ _)) =
+    L.zipWith4 (Vect) xs [y0..y1] (repeat 0) (repeat 1)
     where   xs = map ((+x0) . (`quot` (2* abs dy))) . tail $ [negate dx, dx..]
             dy = y1 - y0
             dx = x1 - x0
